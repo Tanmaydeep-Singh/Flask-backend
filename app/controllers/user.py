bf186c1d27372c1d96cda0f7bf388eaa27a9f5d0
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2023-08-31 15:34:56 +0530

    reset password added

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -13,4 +13,19 @@ def authUser():
 	password = data.get('password')
 	authanticateUser(email, password)
 	return data
+
+@app.route('/login', methods = ['POST'])
+def loginUser():
+	data = request.get_json()
+	email = data.get('email')
+	password = data.get('password')
+	loginUser(email,password)
+	return data
+
+@app.route('/reset-password', methods=['POST'])
+def resetPassword():
+	data = request.get_json()
+	email = data.get('email')
+	reset(email)
+	return data
 	
--- app/features/authentication.py
+++ app/features/authentication.py
@@ -11,7 +11,6 @@ config = {
     "databaseURL":"",
   };
 
-
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
@@ -19,6 +18,9 @@ def authanticateUser(email,password):
     auth.create_user_with_email_and_password(email,password)
     print("SUCCESS")
 
-# user = auth.sign_in_with_email_and_password(email, password)
+def loginUser(email,password):
+    auth.sign_in_with_email_and_password(email, password)
 
-# print(user)
+def reset(email):
+    auth.send_password_reset_email(email)
+   
